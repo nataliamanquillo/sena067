fcc43abcefcb7793010f580a4f4ee334dda65383
nataliamanquillo <125220834+nataliamanquillo@users.noreply.github.com>
2023-07-31 16:18:22 -0500

    Add files via upload

---
+++ pagina web/script.js
@@ -0,0 +1,10 @@
+document.getElementById('guardar').addEventListener('click', function() {
+    var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
+    var telefono = document.getElementById('telefono').value;
+    var correo = document.getElementById('correo').value;
+    var experienciaLaboral = document.getElementById('experiencia-laboral').value;
+    var educacion = document.getElementById('educacion').value;
+  
+    // Aquí puedes realizar las acciones necesarias con los datos ingresados, como guardarlos en una base de datos o generar algún tipo de notificación.
+  });
